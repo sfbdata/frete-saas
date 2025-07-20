111ea52dfe4f190e81602b84f87131da7e036af0
Samuel Freitas <sfb.samuell@gmail.com>
2025-07-20 14:37:21 -0300

    chore: atualiza rotas da API para incluir envio de contato e update de perfil

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,20 +4,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FreteRequestController;
 use App\Http\Controllers\FreteiroProfileController;
+use App\Http\Controllers\ContatoController;
 
+// Rotas públicas
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
-Route::post('/fretes', [FreteRequestController::class, 'store']);
-Route::post('/fretes/request', [FreteRequestController::class, 'store']);
+
 Route::get('/freteiros', [FreteiroProfileController::class, 'index']);
 Route::get('/freteiros/{id}', [FreteiroProfileController::class, 'show']);
-Route::post('/fretes/{frete}/contato/{freteiro}', [\App\Http\Controllers\ContatoController::class, 'enviarContato']);
 
+Route::post('/fretes', [FreteRequestController::class, 'store']);
+Route::post('/fretes/request', [FreteRequestController::class, 'store']); // redundante, mas mantida
 
+Route::post('/fretes/{frete}/contato/{freteiro}', [ContatoController::class, 'enviarContato']);
 
-
+// Rotas protegidas
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me',     [AuthController::class, 'me']);
     Route::post('/logout',[AuthController::class, 'logout']);
+
+    // Criar e atualizar perfil de freteiro
     Route::post('/freteiro-profile', [FreteiroProfileController::class, 'store']);
+    Route::put('/freteiros/{id}',    [FreteiroProfileController::class, 'update']);
 });
